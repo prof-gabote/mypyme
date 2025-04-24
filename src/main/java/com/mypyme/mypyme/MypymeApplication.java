fc1d7db362bab1aa0436960791419f53d2778b5f
Gabriel Serrano <ga.serranom@profesor.duoc.cl>
2025-04-24 10:15:04 -0400

    initial commit

---
+++ src/main/java/com/mypyme/mypyme/MypymeApplication.java
@@ -0,0 +1,13 @@
+package com.mypyme.mypyme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MypymeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MypymeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mypyme/mypyme/MypymeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mypyme.mypyme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MypymeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
